40ef30619904d0f30058fcfdfb5d4e33d3de6296
Alessio <alessiosal1998@gmail.com>
2021-10-18 12:39:07 +0200

    Completed Exercise 4

---
+++ exercises/rp_04_cpp_templates_autodiff/build/CMakeFiles/ad_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ad_test.dir/ad_test.cpp.o"
+  "ad_test"
+  "ad_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ad_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
